d21d97d285a77d86b04d1125ce29194076bc28dc
Odd Stråbø <oddstr13@openshell.no>
2021-08-16 04:46:36 +0200

    Add selinux support to dev.sh

--- dev.sh
+++ dev.sh
@@ -4,7 +4,10 @@ CONTAINER_IMAGE="ghcr.io/oddstr13/docker-tizen-webos-sdk:webos-only"
 MY=$(realpath "$(dirname $0)")
 
 function ct {
-  docker container run -it --rm --user=$UID --network=host -v "${MY}":/home/developer $CONTAINER_IMAGE "$@"
+  # The volume mount flag should be a lower case z.
+  # NEVER upper case, as that could break permissions badly.
+  # https://docs.docker.com/storage/bind-mounts/#configure-the-selinux-label
+  docker container run -it --rm --user=$UID --network=host -v "${MY}":/home/developer:z $CONTAINER_IMAGE "$@"
 }
 
 if [ $# -lt 1 ]; then
