d5ef169cefc6e46f2c2ba3af775ab04022e8dd9e
tuxuser <462620+tuxuser@users.noreply.github.com>
2021-12-08 05:42:17 +0100

    package.json: Add helper scripts to generate metadata & bump version in appinfo.json from package.json

---
+++ org.jellyfin.webos/tools/gen-manifest.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const crypto = require('crypto');
+const fs = require('fs');
+
+const outfile = process.argv[2];
+const appinfo = JSON.parse(fs.readFileSync('appinfo.json'));
+const ipkfile = `build/${appinfo.id}_${appinfo.version}_all.ipk`;
+const ipkhash = crypto.createHash('sha256').update(fs.readFileSync(ipkfile)).digest('hex');
+
+fs.writeFileSync(
+  outfile,
+  JSON.stringify({
+    id: appinfo.id,
+    version: appinfo.version,
+    type: appinfo.type,
+    title: appinfo.title,
+    appDescription: appinfo.appDescription,
+    iconUri: 'https://github.com/tuxuser/jellyfin-webos/raw/master/org.jellyfin.webos/assets/icon-transparent130.png',
+    sourceUrl: 'https://github.com/jellyfin/jellyfin-webos',
+    rootRequired: true,
+    ipkUrl: ipkfile,
+    ipkHash: {
+      sha256: ipkhash,
+    },
+  }),
+);
